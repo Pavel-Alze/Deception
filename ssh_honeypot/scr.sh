4f824361dc131dfa43cdae103916204e5bdebaf3
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2023-12-10 15:44:52 +0300

    Update scr.sh

--- ssh_honeypot/scr.sh
+++ ssh_honeypot/scr.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+until [ -n "$(docker ps --format 'table {{.ID}}\t{{.Names}}' | grep honeypot)" ]
+do
+sleep 10
+done
+
 inotifywait -e modify -m -r $(docker inspect --format='{{.LogPath}}' $(docker ps -aqf "name=ssh_honeypot_ssh"))|\
 (
 while read
