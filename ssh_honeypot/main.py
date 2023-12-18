9f6cd0b83f03e15c43452505fb2c617975fac543
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2023-12-18 21:57:21 +0300

    Delete ssh_honeypot directory

--- ssh_honeypot/main.py
+++
@@ -1,3 +0,0 @@
-token ='6320810642:AAHXboreX9oz3o3T59F4ftjOOyNUJ-RG4AQ'
-import urllib.request
-urllib.request.urlopen('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=-1002004518874&text=Entry+detected!')
