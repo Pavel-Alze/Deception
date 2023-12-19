928fe4d72f0eb4e215d5a46013775d6b9691819a
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2023-12-20 01:02:52 +0300

    Update install.sh

--- ssh_honeypot/install.sh
+++ ssh_honeypot/install.sh
@@ -7,9 +7,9 @@ iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp -s 149.154.167.220 -j ACCEPT
 service netfilter-persistent save
-cp config/main.py /bin/
-cp config/scr.sh /usr/local/bin/
-cp config/on-boot-script.service /etc/systemd/system/
+cp config/main.py /bin/mai.py
+cp config/scr.sh /usr/local/bin/scr.sh
+cp config/on-boot-script.service /etc/systemd/system/on-boot-script.service
 systemctl daemon-reload
 systemctl enable on-boot-script.service
 docker-compose build
