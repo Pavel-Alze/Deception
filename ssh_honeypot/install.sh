604b7d425529799368e12676cbd012f6f51b80f6
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2023-12-19 17:49:19 +0300

    Update install.sh

--- ssh_honeypot/install.sh
+++ ssh_honeypot/install.sh
@@ -6,7 +6,6 @@ apt-get install -y inotify-tools
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp -s 149.154.167.220 -j ACCEPT
-iptables -A INPUT -p tcp -j REJECT
 service netfilter-persistent save
 cp config/main.py /bin/
 cp config/scr.sh /usr/local/bin/
