625d97c757ad2263289bc53d6056c1c2c5dcbff8
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2024-01-11 22:45:10 +0300

    Update server.py

--- web/server.py
+++ web/server.py
@@ -5,22 +5,22 @@ app = FastAPI()
 
 @app.get("/", status_code=404)
 def rootNotFound():
-    print("get / api")
+    print("INFO WEB get / api")
     return {"message": "Resource Not Found"}
 
 @app.get("/main", status_code=404)
 def mainNotFound():
-    print("get /main api")
+    print("INFO WEB get /main api")
     return {"message": "Resource Not Found"}
 
 @app.get("/login", status_code=404)
 def loginNotFound():
-    print("get /login api")
+    print("INFO WEB get /login api")
     return {"message": "Resource Not Found"}
 
 @app.get("/admin", status_code=404)
 def adminNotFound():
-    print("get /admin api")
+    print("INFO WEB get /admin api")
     return {"message": "Resource Not Found"}
 
 uvicorn.run(app, host='0.0.0.0', port=8000)
