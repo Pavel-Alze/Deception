be52cf634504e96ba1e47bca191e41abbfdada06
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2024-01-11 22:49:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 apt-get install -y iptables
 apt-get install -y iptables-persistent
 apt-get install -y inotify-tools
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -j ACCEPT
-iptables -A INPUT -p tcp -s 149.154.167.220 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 22 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 21 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 8000 -j ACCEPT
+iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
+iptables -I INPUT 1 -p tcp -s 149.154.167.220 -j ACCEPT
 service netfilter-persistent save
 cp config/main.py /bin/
 cp config/scr.sh /usr/local/bin/
