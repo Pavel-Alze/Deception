1529b62d41ef7877345372b5915e240246401ca3
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2024-01-11 19:15:48 +0300

    Update scr.sh

--- config/scr.sh
+++ config/scr.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-until [ -n "$(docker ps --format 'table {{.ID}}\t{{.Names}}' | grep honeypot)" ]
+until [ -n "$(docker ps --format 'table {{.ID}}\t{{.Names}}' | grep sshpot)" ]
 do
 sleep 10
 done
 
-inotifywait -e modify -m -r $(docker inspect --format='{{.LogPath}}' $(docker ps -aqf "name=honeypot"))|\
+inotifywait -e modify -m -r $(docker inspect --format='{{.LogPath}}' $(docker ps -aqf "name=sshpot"))|\
 (
 while read
 do
