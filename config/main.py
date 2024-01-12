f575b7154b654dac98eaea711d0e0d81f269a6df
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2024-01-12 13:34:36 +0300

    Update main.py

--- config/main.py
+++ config/main.py
@@ -1,3 +1,4 @@
 token ='6320810642:AAHXboreX9oz3o3T59F4ftjOOyNUJ-RG4AQ'
+chat_id ='-1002004518874'
 import urllib.request
-urllib.request.urlopen('https://api.telegram.org/bot'+token+'/sendMessage?chat_id=-1002004518874&text=Entry+detected!')
+urllib.request.urlopen('https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text=Entry+detected!')
